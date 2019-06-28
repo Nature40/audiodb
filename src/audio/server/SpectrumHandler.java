ae66ce79ed545ffa73ac2ea89955eabd419882c8
swoellauer <swoellauer>
2019-06-28 08:41:15 +0200

    color spectrogram, log scale, threshold, raw pixel processing

--- src/audio/server/SpectrumHandler.java
+++ src/audio/server/SpectrumHandler.java
@@ -1,14 +1,10 @@
 package audio.server;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,6 +18,9 @@ import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.jtransforms.fft.FloatFFT_1D;
+
+import util.image.ImageRGBA;
+import util.image.Lut;
 
 public class SpectrumHandler extends AbstractHandler {
 	static final Logger log = LogManager.getLogger();
@@ -39,6 +38,7 @@ public class SpectrumHandler extends AbstractHandler {
 			log.info("FrameLength: " + in.getFrameLength());
 
 			int cutoff = 320;
+			float threshold = 12;
 
 			int n = 1024;
 			int step = 256;
@@ -53,36 +53,64 @@ public class SpectrumHandler extends AbstractHandler {
 			FloatFFT_1D fft = new FloatFFT_1D(n);
 			float[] weight = getGaussianWeights(n);
 
-			BufferedImage bi = new BufferedImage(cols, cutoff, 1);
-			Graphics2D g = bi.createGraphics();
+			float[][] transformed = new float[cols][n];
+			float minv = Float.MAX_VALUE;
+			float maxv = -Float.MAX_VALUE;
 			for (int pos = 0; pos < cols; pos++) {
-				float[] a = new float[n];
+				float[] a = transformed[pos];
 				for (int i = 0; i < n; i++) {
-					//a[i] = fullShorts[pos*step + i];
 					a[i] = fullShorts[pos*step + i] * weight[i];
 				}
 				fft.realForward(a);
 				for (int i = 0; i < cutoff; i++) {
-					//float v = Math.abs(a[i*2+1]) / 40000f;
-					float v = (float) Math.pow(Math.abs(a[i*2+1]), 0.5d) / 200f;
-					if(v < 0f) {
-						v = 0f;
+					float v = (float) Math.log(a[i*2]*a[i*2]+a[i*2+1]*a[i*2+1] + Float.MIN_VALUE);
+					if(v >= threshold) {
+						if(v < minv) {
+							minv = v;
+						}
+						if(maxv < v) {
+							maxv = v;
+						}
+					}					
+				}
+			}
+			
+			/*float rangev = maxv - minv;			
+			BufferedImage bi = new BufferedImage(cols, cutoff, 1);
+			Graphics2D g = bi.createGraphics();
+			for (int pos = 0; pos < cols; pos++) {
+				float[] a = transformed[pos];
+				for (int i = 0; i < cutoff; i++) {
+					float v = (float) Math.log(a[i*2]*a[i*2]+a[i*2+1]*a[i*2+1] + Float.MIN_VALUE);
+					if(v < threshold) {
+						v = 0;
 					}
-					if(v > 1f) {
-						v = 1f;
+					float y = (v - minv) / rangev;
+					if(y < 0f) {
+						y = 0f;
 					}
-					g.setColor(new Color(v, v, v));				
+					if(y > 1f) {
+						y = 1f;
+					}
+					g.setColor(new Color(y, y, y));
 					g.drawLine(pos , cutoff - i - 1, pos, cutoff - i);
 				}
 			}
 			g.dispose();
-			ImageIO.write(bi, "png", response.getOutputStream());
-
-			/*log.info(Arrays.toString(a));
-			fft.realForward(a);
-			log.info(Arrays.toString(a));
-			fft.realInverse(a, true);
-			log.info(Arrays.toString(a));*/
+			ImageIO.write(bi, "png", response.getOutputStream());*/
+			
+			float[] lut = Lut.getGammaLUT256f(minv, maxv, 1.2);			
+			ImageRGBA image = new ImageRGBA(cols, cutoff);
+			int[] dst = image.getRawArray();
+			for (int pos = 0; pos < cols; pos++) {
+				float[] a = transformed[pos];
+				for (int i = 0; i < cutoff; i++) {
+					float v = (float) Math.log(a[i*2]*a[i*2]+a[i*2+1]*a[i*2+1] + Float.MIN_VALUE);
+					int c = Renderer.colInferno[Lut.match256f(lut, v)];
+					dst[(cutoff - i - 1) * cols + pos] = c;
+				}
+			}
+			image.writePngCompressed(response.getOutputStream());
 
 		} catch (UnsupportedAudioFileException e) {
 			throw new RuntimeException(e);
@@ -93,7 +121,7 @@ public class SpectrumHandler extends AbstractHandler {
 		double upper = n;
 		double mean = (upper - 1d) / 2d;
 		
-		Gaussian gaussian = new Gaussian(mean, upper / 4d);
+		Gaussian gaussian = new Gaussian(mean, upper / 5d);
 		
 		int len = (int) upper;
 		int mid = len / 2;
